219ad9763d486a78eca46dccc3ae31b1edab0f88
Paige Mabbitt <PMabbitt992@west-mec.org>
2019-04-02 15:24:38 -0700

    First Commit
    File author, date, and names

--- sub_cart.js
+++ sub_cart.js
@@ -5,8 +5,8 @@
    Tutorial 12
    Case Problem 2
 
-   Author: 
-   Date:   
+   Author: Paige Mabbitt
+   Date:  4.2.19 
 
    Filename: sub_cart.js
 
@@ -21,4 +21,3 @@
       the customer.
 
 */
-
