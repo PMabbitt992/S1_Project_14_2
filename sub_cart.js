f4d8d925ed0b9021d082ed5c87766bf4c52aeccc
Paige Mabbitt <PMabbitt992@west-mec.org>
2019-04-05 13:36:12 -0700

    Duplicate order numbers and comments
    For loop that checks to see if the id is the same as the previous, comments

--- sub_cart.js
+++ sub_cart.js
@@ -21,29 +21,51 @@
       the customer.
 
 */
+//Load setupCart on window load
 window.onload = setupCart;
 
+
 function setupCart() {
+      //Set addButtons to the elemnts with class addButton 
       var addButtons = document.getElementsByClassName('addButton');
+      //For each item in addButtons, and a click event listener
       for (var i = 0; i < addButtons.length; i++) {
             addButtons[i].addEventListener("click", addItem);
       }
 }
 
 function addItem(e) {
+      //Food item is equal to the target buttons next elemnt
       var foodItem = e.target.nextElementSibling;
+      //Food ID is equal to the food item ID
       var foodID = foodItem.id;
+      // food description clones food item for use in the cart
       var foodDescription = foodItem.cloneNode(true);
+      //cartBox is the element where the cart items will appear
       var cartBox = document.getElementById("cart");
+      //diplucateOrder default false
       var duplicateOrder = false;
 
-      //check current id against past food id to see if there is a duplicate. If yes, add 1 to orderCount(?) and skip duplicate order
- 
+      //for each last element after cartBox that is not null...
+      for (var n = cartBox.firstElementChild; n = n.nextElementSibling; n !== null) {
+            //...check if the id is equal to foodID
+            if (n.id === foodID) {
+                  //if so, add to the text content of the first child and set duplicate order to true
+                  duplicateOrder = true;
+                  n.firstElementChild.textContent++;
+                  break;
+            }
+      }
+
       if (duplicateOrder === false) {
+            //if duplicate order is still false, create a new order count box and add it and the food description to cartBox
             var orderCount = document.createElement("span");
             orderCount.textContent = 1;
+            foodDescription.prepend(orderCount);
+            cartBox.appendChild(foodDescription);
       }
-      foodDescription.prepend(orderCount);
-      cartBox.appendChild(foodDescription); 
-   
+
+
+
+
 }
